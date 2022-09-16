4db0343742492d90f6dfcb59702d9f52b022609c
kenechukwu Emmanuel <97654031+codewithkaycee@users.noreply.github.com>
2022-09-16 16:27:34 +0100

    Create Multiply.js

---
+++ Multiply.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  return a * b
+}
+
+// Another simple way to solve the kata
+//Arrow function
+ multiply = (a, b) => a * b;
