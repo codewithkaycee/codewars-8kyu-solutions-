63505fc02a273eb31dd325114a39a19c168f8a81
kenechukwu Emmanuel <97654031+codewithkaycee@users.noreply.github.com>
2022-09-16 16:44:45 +0100

    Create your first function and print "Hello world".js

---
+++ Training JS #1: create your first JS function and print "Hello World!".js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+// Look at this example:
+
+//       --------keyword "function"
+//       |       ----your function name  
+//       |       |    ---if needed, some arguments will appear in parentheses
+//       |       |    |
+//     function myfunc(){  ---------your function code will starting with "{"
+//       //you should type your code here
+//     }----------------------------ending with "}"
+
+// SOLUTION
+function helloWorld(){
+  let str = "Hello World!"
+  console.log(str)
+}
