e5a3a10111e5a38b01208d6f62dce240f202a2f0
kenechukwu Emmanuel <97654031+codewithkaycee@users.noreply.github.com>
2022-09-18 17:12:54 +0100

    Create You Can't Code Under Pressure #1.js

---
+++ You Can't Code Under Pressure #1.js
@@ -0,0 +1,14 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
+// Another way to solve this challenge.
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  i += i
+  return i ;
+}
