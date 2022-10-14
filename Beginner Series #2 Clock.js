a5fea1fc710bf54c04b67908fa493e9581c5616b
kenechukwu Emmanuel <97654031+codewithkaycee@users.noreply.github.com>
2022-10-14 23:48:14 +0100

    Create Beginner Series #2 Clock.js

--- Beginner Series #2 Clock.js
+++ Beginner Series #2 Clock.js
@@ -14,3 +14,13 @@
 // 0 <= h <= 23
 // 0 <= m <= 59
 // 0 <= s <= 59
+
+
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  let hours = h * 60 * 60 * 1000
+  let minutes = m * 60 * 1000
+  let seconds = s * 1000
+  return hours + minutes + seconds
+  
+}
